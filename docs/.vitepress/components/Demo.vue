d0a4d0767880ad71136402b9d6b987fcd4de8486
rose79 <lioxbb@outlook.com>
2024-08-07 14:15:51 +0800

    feat: 添加组件库文档&演练场&demo

---
+++ docs/.vitepress/components/Demo.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useData } from 'vitepress';
+
+const props = withDefaults(defineProps<{
+  /** 用例源码 */
+  source?: string;
+}>(), {
+  source: '',
+});
+
+const MAIN_FILE_NAME = 'App.vue';
+
+const { site } = useData();
+
+// 将用例源码按照 Playground 的规则转换为 Base64 编码
+const sourceHash = computed(() => {
+  const originCode = {
+    [MAIN_FILE_NAME]: decodeURIComponent(props.source),
+  };
+  return btoa(unescape(encodeURIComponent(JSON.stringify(originCode))));
+});
+
+// 跳转到 Playground，将 Base64 编码作为 hash 参数，Playground 页面就能展示对应的用例源码
+function toPlayground() {
+  window.open(
+    `${window.location.origin}${site.value.base}playground.html#${sourceHash.value}`,
+    '_blank',
+  );
+}
+
+const isCodeShow = ref(false);
+</script>
+
+<template>
+  <div class="demo">
+    <div class="demo-render">
+      <slot name="demo" />
+    </div>
+    <div class="demo-operators">
+      <i class="i-au-code-screen" title="在 Playground 中编辑" @click="toPlayground" />
+      <i class="i-au-code" title="查看源代码" @click="isCodeShow = !isCodeShow" />
+    </div>
+    <div v-if="isCodeShow" class="demo-code">
+      <slot name="code" />
+      <div class="pb-16px text-center" @click="isCodeShow = false">
+        <a href="javascript:;" class="cursor-pointer c-info! no-underline!">隐藏源代码</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.demo {
+  border: 1px solid rgb(var(--au-color-bd_light));
+  border-radius: 4px;
+}
+
+.demo-render {
+  padding: 20px;
+}
+
+.demo-operators {
+  display: flex;
+  justify-content: flex-end;
+  padding: 16px;
+  font-size: 18px;
+  color: rgb(var(--au-color-secondary));
+  border-top: 1px solid rgb(var(--au-color-bd_light));
+
+  i {
+    cursor: pointer;
+
+    + i {
+      margin-left: 16px;
+    }
+  }
+}
+</style>
---
+++ docs/.vitepress/env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+declare module 'markdown-it-container' {
+  import type { PluginWithParams } from 'markdown-it';
+
+  const container: PluginWithParams;
+  export = container;
+}
